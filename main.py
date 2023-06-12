7bd22a8ae0cfc5ba2b4c4c933a3d0b5454767dcd
Elkarapuzik <shmakov-10@bk.ru>
2023-06-12 11:04:57 +0300

    formated code

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ from flask import Flask
 
 
 def fetch_coordinates(yandex_geocoder_apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
+    base_url = 'https://geocode-maps.yandex.ru/1.x'
     response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": yandex_geocoder_apikey,
-        "format": "json",
+        'geocode': address,
+        'apikey': yandex_geocoder_apikey,
+        'format': 'json',
     })
     response.raise_for_status()
     found_places = response.json()['response']['GeoObjectCollection']['featureMember']
@@ -23,21 +23,21 @@ def fetch_coordinates(yandex_geocoder_apikey, address):
         return None
 
     most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(' ')
     return lat, lon
 
 
 #________________________________________________________________________________________________#    
-yandex_geocoder_apikey = ("ef14f529-5559-43da-8ee6-5ae1ed765b47")
+yandex_geocoder_apikey = ('ef14f529-5559-43da-8ee6-5ae1ed765b47')
 #просто ключ от яндекс геоокодера / yandex geocoder key
 
-person_location  = input("Где вы находитесь?")
+person_location  = input('Где вы находитесь?')
 person_coords = fetch_coordinates(yandex_geocoder_apikey , person_location )
 #Местонахождение человека (безкоорд)/Location of a person(without coordinates)
 
 
 #_______________________________________________________________________________
-with open("coffee.json", "r", encoding='UTF-8') as my_file:
+with open('coffee.json', 'r', encoding='UTF-8') as my_file:
   file_contents = my_file.read()
 coffee_shops = json.loads(file_contents)#Из текстого формата в формат списка словарей
 #Работа с файлом/Working with a file
@@ -46,66 +46,92 @@ coffee_shops = json.loads(file_contents)#Из текстого формата в
 #_______________________________________________________________________________
 coffee_shops_with_distance = []
 for coffee in coffee_shops:
-  coffee_grab = {'title'  :            coffee['Name'] ,
-              'latitude'  :  coffee['Latitude_WGS84'] ,
-              'longitude' : coffee['Longitude_WGS84'] ,
-              'distance'  :(distance.distance(person_coords,(coffee['Latitude_WGS84'] , coffee['Longitude_WGS84'])).km),
-             }
-  coffee_shops_with_distance.append(coffee_grab)
+    coffee_grab = {
+        'title': coffee['Name'] ,
+        'latitude': coffee['Latitude_WGS84'] ,
+        'longitude': coffee['Longitude_WGS84'] ,
+        'distance': distance.distance(
+           person_coords,
+           (coffee['Latitude_WGS84'] , coffee['Longitude_WGS84'])
+        ).km,
+    }
+    coffee_shops_with_distance.append(coffee_grab)
 #Собственный список с кафешками/Own list with cafes
 
 
 #________________________________________________________________________________
 def get_user_posts(user):
     return user['distance']
-distance_sorted_coffee_shops =  sorted(coffee_shops_with_distance, key=get_user_posts)
+distance_sorted_coffee_shops = sorted(
+    coffee_shops_with_distance,
+    key=get_user_posts
+)
 five_nearest_coffee_shops = distance_sorted_coffee_shops[0:5] 
 #Ближайшая кофейня/Nearest coffee shop
 
 
 #_________________________________________________________________________________
 person_reversed_coords = [person_coords[0],person_coords[1]]
-map = folium.Map(location = person_reversed_coords,zoom_start=13, tiles="Stamen Terrain")
+map = folium.Map(
+    location = person_reversed_coords,
+    zoom_start=13,
+    tiles='Stamen Terrain'
+)
 
-tooltip = "Click me!"
+tooltip = '''I'm here'''
 
 folium.Marker(
-    [five_nearest_coffee_shops[0]['latitude'],five_nearest_coffee_shops[0]['longitude']],
+    [
+        five_nearest_coffee_shops[0]['latitude'],
+        five_nearest_coffee_shops[0]['longitude']
+    ],
     popup=five_nearest_coffee_shops[0]['title'],
-    icon=folium.Icon(color="green", icon="info-sign"),
+    icon=folium.Icon(color='green', icon='info-sign'),
 ).add_to(map)
 
 folium.Marker(
-    [five_nearest_coffee_shops[1]['latitude'],five_nearest_coffee_shops[1]['longitude']],
+    [
+        five_nearest_coffee_shops[1]['latitude'],
+        five_nearest_coffee_shops[1]['longitude']
+    ],
     popup=five_nearest_coffee_shops[1]['title'],
-    icon=folium.Icon(color="green", icon="info-sign"),
+    icon=folium.Icon(color='green', icon='info-sign'),
 ).add_to(map)
 
 folium.Marker(
-    [five_nearest_coffee_shops[2]['latitude'],five_nearest_coffee_shops[2]['longitude']],
+    [
+        five_nearest_coffee_shops[2]['latitude'],
+        five_nearest_coffee_shops[2]['longitude']
+    ],
     popup=five_nearest_coffee_shops[2]['title'],
-    icon=folium.Icon(color="green", icon="info-sign"),
+    icon=folium.Icon(color='green', icon='info-sign'),
 ).add_to(map)
 
 folium.Marker(
-    [five_nearest_coffee_shops[3]['latitude'],five_nearest_coffee_shops[3]['longitude']],
+    [
+        five_nearest_coffee_shops[3]['latitude'],
+        five_nearest_coffee_shops[3]['longitude']
+    ],
     popup=five_nearest_coffee_shops[3]['title'],
-    icon=folium.Icon(color="green", icon="info-sign"),
+    icon=folium.Icon(color='green', icon='info-sign'),
 ).add_to(map)
 
 folium.Marker(
-    [five_nearest_coffee_shops[4]['latitude'],five_nearest_coffee_shops[4]['longitude']],
+    [
+        five_nearest_coffee_shops[4]['latitude'],
+        five_nearest_coffee_shops[4]['longitude']
+    ],
     popup=five_nearest_coffee_shops[4]['title'],
-    icon=folium.Icon(color="green", icon="info-sign"),
+    icon=folium.Icon(color='green', icon='info-sign'),
 ).add_to(map)
 
 folium.Marker(
     person_reversed_coords,
-    popup="Ваше местонахождение",
-    icon=folium.Icon(color="red", icon="info-sign"),
+    popup='Ваше местонахождение',
+    icon=folium.Icon(color='red', icon='info-sign'),
 ).add_to(map)
 
-map.save("index.html")
+map.save('index.html')
 #Работа с картой/Work with map
 
 
